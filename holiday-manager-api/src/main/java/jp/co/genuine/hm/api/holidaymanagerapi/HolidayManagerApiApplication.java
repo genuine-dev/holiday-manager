fbf114e97d364b978ead23201c5cde4f53ca71b3
Tomohiro Saito <saito@.com>
2019-11-18 21:35:51 +0900

    空プロジェクト作成

---
+++ holiday-manager-api/src/main/java/jp/co/genuine/hm/api/holidaymanagerapi/HolidayManagerApiApplication.java
@@ -0,0 +1,13 @@
+package jp.co.genuine.hm.api.holidaymanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayManagerApiApplication.class, args);
+	}
+
+}
---
+++ holiday-manager-api/src/test/java/jp/co/genuine/hm/api/holidaymanagerapi/HolidayManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.genuine.hm.api.holidaymanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
