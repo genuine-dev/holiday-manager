1be4870d8505b4d66d9293842411ffcd15dacfd0
TomohiroSaito <saito@genuine-pt.jp>
2020-12-05 11:12:22 +0900

    holiday-manager-apiをプロジェクト直下のpomに追加 コンパイルエラーを解決

--- holiday-manager-api/src/test/java/jp/co/genuine/hm/api/HolidayManagerApiApplicationTests.java
+++ holiday-manager-api/src/test/java/jp/co/genuine/hm/api/HolidayManagerApiApplicationTests.java
@@ -1,12 +1,10 @@
 package jp.co.genuine.hm.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HolidayManagerApiApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
