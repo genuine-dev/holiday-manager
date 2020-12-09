65731a0b59f4720077382dafc0850060c8288b6f
TomohiroSaito <saito@genuine-pt.jp>
2020-12-09 11:16:11 +0900

    ビルドエラーの解決

--- holiday-manager-api/src/main/java/jp/co/genuine/hm/api/configuration/WebSecurityConfig.java
+++ holiday-manager-api/src/main/java/jp/co/genuine/hm/api/configuration/WebSecurityConfig.java
@@ -2,7 +2,6 @@ package jp.co.genuine.hm.api.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,7 +9,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.AccessDeniedHandler;
@@ -27,6 +25,9 @@ import jp.co.genuine.hm.api.security.SimpleAuthenticationSuccessHandler;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	PasswordEncoder PasswordEncoder;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -68,11 +69,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.eraseCredentials(true)
 			.userDetailsService(userDetailsService)
 			.passwordEncoder(passwordEncoder);
-	}
-
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
 	}
 
 	private LogoutSuccessHandler logoutSuccessHandler() {
