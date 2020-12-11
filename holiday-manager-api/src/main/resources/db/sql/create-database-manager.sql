07e9a933f101873736f4a3e3be991ab95743385c
TomohiroSaito <saito@genuine-pt.jp>
2020-12-12 08:30:07 +0900

    flywayによるDBマイグレーション実装

---
+++ holiday-manager-api/src/main/resources/db/sql/create-database-manager.sql
@@ -0,0 +1,10 @@
+-- DROP DATABASE manager;
+
+CREATE DATABASE manager
+    WITH
+    OWNER = manager
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Japanese_Japan.932'
+    LC_CTYPE = 'Japanese_Japan.932'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
