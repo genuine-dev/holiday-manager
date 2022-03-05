bd972297066252b97a21488060d934c60f98502e
TomohiroSaito <saito@genuine-pt.jp>
2022-03-05 10:36:48 +0900

    有給管理API リファクタリング #41 requestmappingアノテーション変更

--- holiday-manager-api/src/main/java/holiday/manager/controller/holiday/HolidayController.java
+++ holiday-manager-api/src/main/java/holiday/manager/controller/holiday/HolidayController.java
@@ -6,10 +6,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,14 +32,14 @@ public class HolidayController {
 	HolidayService holidayService;
 
 	@ApiOperation("有給取得アラート")
-	@RequestMapping(path = "/holiday/alert/{user_id}", method = RequestMethod.GET)
+	@GetMapping("/holiday/alert/{user_id}")
 	public ResponseEntity<HolidayAlert> getHolidayAlert(@PathVariable("user_id") String userId) {
 		HolidayAlert holidayAlert = holidayService.getHolidayAlert(userId);
 		return new ResponseEntity<HolidayAlert>(holidayAlert, HttpStatus.OK);
 	}
 
 	@ApiOperation("有給残日数取得")
-	@RequestMapping(path = "/holiday/{user_id}/days", method = RequestMethod.GET)
+	@GetMapping("/holiday/{user_id}/days")
 	public ResponseEntity<Double> getHolidayDays(@PathVariable("user_id") String userId,
 			@RequestParam(name = "kind", defaultValue = "PAYED_LEAVE") String kind) {
 		Double days = holidayService.getHolidayDays(userId, kind);
@@ -45,7 +47,7 @@ public class HolidayController {
 	}
 
 	@ApiOperation("ステータスから有給申請リスト取得(全取得はALLを指定)")
-	@RequestMapping(path = "/holiday/{user_id}/application", method = RequestMethod.GET)
+	@GetMapping("/holiday/{user_id}/application")
 	public ResponseEntity<List<HolidayApplication>> getHolidayApplication(@PathVariable("user_id") String userId,
 			@RequestParam(name = "status", defaultValue = "ALL") String status) {
 		List<HolidayApplication> holidayApplications = holidayService.getHolidayApplication(userId, status);
@@ -53,14 +55,14 @@ public class HolidayController {
 	}
 
 	@ApiOperation("承認待ち有給申請リスト取得")
-	@RequestMapping(path = "/holiday/applying/{applover_id}", method = RequestMethod.GET)
+	@GetMapping("/holiday/applying/{applover_id}")
 	public ResponseEntity<List<HolidayApplication>> getApplyingHoliday(@PathVariable("applover_id") String userId) {
 		List<HolidayApplication> applyingHolidays = holidayService.getApplyingHoliday(userId);
 		return new ResponseEntity<List<HolidayApplication>>(applyingHolidays, HttpStatus.OK);
 	}
 
 	@ApiOperation("有給付与")
-	@RequestMapping(path = "/holiday/grant", method = RequestMethod.POST)
+	@PostMapping("/holiday/grant")
 	public ResponseEntity<HolidayList> postHolidayGrant(@RequestBody PostHolidayGrantRequest request)
 			throws ParseException {
 		HolidayList holidayList = holidayService.postHolidayGrant(request);
@@ -68,7 +70,7 @@ public class HolidayController {
 	}
 
 	@ApiOperation("有給申請")
-	@RequestMapping(path = "/holiday/apply", method = RequestMethod.POST)
+	@PostMapping("/holiday/apply")
 	public ResponseEntity<HolidayApplication> postHolidayApply(@RequestBody PostHolidayApplyRequest request)
 			throws ParseException {
 		HolidayApplication application = holidayService.postHolidayApply(request);
@@ -76,21 +78,21 @@ public class HolidayController {
 	}
 
 	@ApiOperation("有給承認")
-	@RequestMapping(path = "/holiday/approve", method = RequestMethod.PUT)
+	@PutMapping("/holiday/approve")
 	public ResponseEntity<HolidayApplication> putHolidayApprove(@RequestBody PutHolidayApproveRequest request) {
 		HolidayApplication application = holidayService.putHolidayApprove(request);
 		return new ResponseEntity<HolidayApplication>(application, HttpStatus.OK);
 	}
 
 	@ApiOperation("有給却下")
-	@RequestMapping(path = "/holiday/reject", method = RequestMethod.DELETE)
+	@DeleteMapping("/holiday/reject")
 	public ResponseEntity<HolidayApplication> deleteHolidayReject(@RequestBody DeleteHolidayRejectRequest request) {
 		HolidayApplication application = holidayService.deleteHolidayReject(request);
 		return new ResponseEntity<HolidayApplication>(application, HttpStatus.OK);
 	}
 
 	@ApiOperation("有給キャンセル")
-	@RequestMapping(path = "/holiday/cancel", method = RequestMethod.DELETE)
+	@DeleteMapping("/holiday/cancel")
 	public ResponseEntity<HolidayApplication> deleteHolidayCancel(@RequestBody DeleteHolidayCancelRequest request) {
 		HolidayApplication application = holidayService.deleteHolidayCancel(request);
 		return new ResponseEntity<HolidayApplication>(application, HttpStatus.OK);
--- holiday-manager-api/src/main/java/holiday/manager/controller/user/UserController.java
+++ holiday-manager-api/src/main/java/holiday/manager/controller/user/UserController.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +40,7 @@ public class UserController {
 	UserService userService;
 
 	@ApiOperation("全ユーザー取得")
-	@RequestMapping(path = "/user", method = RequestMethod.GET)
+	@GetMapping("/user")
 	public ResponseEntity<UserList> getUsers(
 			@ContainsUserSortType @RequestParam(value = "sort", required = false) List<UserSort> sortList,
 			@RequestParam(value = "queryusername", required = false) String userNameQuery) {
@@ -47,42 +49,42 @@ public class UserController {
 	}
 
 	@ApiOperation("ユーザー登録")
-	@RequestMapping(path = "/user", method = RequestMethod.POST)
+	@PostMapping("/user")
 	@ResponseStatus(HttpStatus.OK)
 	public void postUser(@RequestBody @Valid PostUserRequest request) {
 		userService.postUser(request);
 	}
 
 	@ApiOperation("ユーザー更新")
-	@RequestMapping(path = "/user/{user_id}", method = RequestMethod.PUT)
+	@PutMapping("/user/{user_id}")
 	@ResponseStatus(HttpStatus.OK)
 	public void putUser(@PathVariable("user_id") @Valid UserId userId, @RequestBody @Valid PutUserRequest request) {
 		userService.putUser(userId, request);
 	}
 
 	@ApiOperation("ユーザー削除")
-	@RequestMapping(path = "/user/{user_id}", method = RequestMethod.DELETE)
+	@DeleteMapping("/user/{user_id}")
 	@ResponseStatus(HttpStatus.OK)
 	public void deleteUser(@PathVariable("user_id") @Valid UserId userId) {
 		userService.deleteUser(userId);
 	}
 
 	@ApiOperation("UserId（DBのシーケンス番号）から特定の一人の情報を取得")
-	@RequestMapping(path = "/user/{user_id}", method = RequestMethod.GET)
+	@GetMapping("/user/{user_id}")
 	public ResponseEntity<User> getUser(@PathVariable("user_id") @Valid UserId userId) {
 		User user = userService.findUser(userId);
 		return new ResponseEntity<User>(user, HttpStatus.OK);
 	}
 
 	@ApiOperation("ユーザーステータスのキーとバリュー一覧取得")
-	@RequestMapping(path = "/user/status", method = RequestMethod.GET)
+	@GetMapping("/user/status")
 	public ResponseEntity<Map<String, String>> getUserStatus() {
 		Map<String, String> userStatusMap = userService.getUserStatus();
 		return new ResponseEntity<Map<String, String>>(userStatusMap, HttpStatus.OK);
 	}
 
 	@ApiOperation("DBにアカウントIDが存在するかチェック")
-	@RequestMapping(path = "/exist/accountId/{account_id}", method = RequestMethod.GET)
+	@GetMapping("/exist/accountId/{account_id}")
 	public ResponseEntity<Boolean> getExistAccountId(@PathVariable("account_id") AccountId accountId) {
 		Boolean existAccountId = userService.existAccountId(accountId);
 		return new ResponseEntity<Boolean>(existAccountId, HttpStatus.OK);
