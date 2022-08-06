167fbdd40a18d9075ff95227f6a8fb90dcdc49a6
nakamura <nakamura@genuine-pt.jp>
2022-08-06 21:40:45 +0900

    Javadoc追加

--- holiday-manager-core/src/main/java/holiday/manager/application/service/holiday/application/HolidayApplicationService.java
+++ holiday-manager-core/src/main/java/holiday/manager/application/service/holiday/application/HolidayApplicationService.java
@@ -26,6 +26,14 @@ public class HolidayApplicationService {
 	@Autowired
 	private HolidayApplicationQueryRepository queryRepository;
 
+	/**
+	 * 休暇申請を申請する
+	 * @param kindOfHoliday 休暇の種別
+	 * @param holidayType　休暇のタイプ
+	 * @param date 休暇取得日
+	 * @param applicant 申請者
+	 * @return 休暇申請
+	 */
 	public HolidayApplication apply(KindOfHoliday kindOfHoliday, HolidayType holidayType, Date date, User applicant) {
 		HolidayApplication application = new HolidayApplication(kindOfHoliday, holidayType, date, applicant.getId());
 		repository.save(application);
@@ -33,6 +41,12 @@ public class HolidayApplicationService {
 		return application;
 	}
 
+	/**
+	 * 休暇申請を承認する
+	 * @param id 休暇申請ID
+	 * @param approver 承認者
+	 * @return 休暇申請
+	 */
 	public HolidayApplication approve(HolidayApplicationId id, User approver) {
 		HolidayApplication application = repository.findById(id);
 		application.approve(approver);
@@ -41,6 +55,12 @@ public class HolidayApplicationService {
 		return application;
 	}
 
+	/**
+	 * 休暇申請を却下する
+	 * @param id 休暇申請ID
+	 * @param approver 承認者
+	 * @return 休暇申請
+	 */
 	public HolidayApplication reject(HolidayApplicationId id, User approver) {
 		HolidayApplication application = repository.findById(id);
 		application.reject(approver);
@@ -49,6 +69,12 @@ public class HolidayApplicationService {
 		return application;
 	}
 
+	/**
+	 * 休暇申請を取り消す
+	 * @param id 休暇申請ID
+	 * @param applecant 申請者
+	 * @return 休暇申請
+	 */
 	public HolidayApplication cancel(HolidayApplicationId id, User applecant) {
 		HolidayApplication application = repository.findById(id);
 		application.cancel(applecant);
@@ -59,6 +85,11 @@ public class HolidayApplicationService {
 
 	}
 
+	/**
+	 * 休暇申請を処理済みにする
+	 * @param id 休暇申請ID
+	 * @return 休暇申請
+	 */
 	public HolidayApplication process(HolidayApplicationId id) {
 		HolidayApplication application = repository.findById(id);
 		application.process();
@@ -69,6 +100,11 @@ public class HolidayApplicationService {
 
 	}
 
+	/**
+	 * 休暇申請を処理失敗にする
+	 * @param id 休暇申請ID
+	 * @return 休暇申請
+	 */
 	public HolidayApplication processFail(HolidayApplicationId id) {
 		HolidayApplication application = repository.findById(id);
 		application.processFail();
