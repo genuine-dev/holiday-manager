7627aef81966741ccc1d43586034df8efea1a143
TomohiroSaito <>
2022-11-04 07:57:32 +0900

    WEB作成 #63 RestTemplateを使用するように修正

--- holiday-manager-web/src/main/java/jp/co/genuine/hm/service/user/UserService.java
+++ holiday-manager-web/src/main/java/jp/co/genuine/hm/service/user/UserService.java
@@ -1,20 +1,17 @@
 package jp.co.genuine.hm.service.user;
 
-import java.io.IOException;
-import java.util.Map;
-
-import org.apache.http.client.methods.CloseableHttpResponse;
-
 import jp.co.genuine.hm.model.user.User;
 import jp.co.genuine.hm.model.user.UserId;
 import jp.co.genuine.hm.model.user.UserList;
 import jp.co.genuine.hm.model.user.UserViewModel;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
 
 public interface UserService {
 	public UserList getUserList() throws IOException;
-	public User getUser(UserId userId) throws IOException;
-	public CloseableHttpResponse postUser(UserViewModel parameter) throws IOException;
-	public CloseableHttpResponse putUser(UserId userId, UserViewModel parameter)throws IOException;
-	public CloseableHttpResponse deleteUser(UserId userId)throws IOException;
-	public Map<String, String> getUserStatus() throws IOException;
+	public User getUser(UserId userId);
+	public ResponseEntity<Void> postUser(UserViewModel parameter);
+	public ResponseEntity<Void> putUser(UserId userId, UserViewModel parameter)throws IOException;
+	public ResponseEntity<Void> deleteUser(UserId userId)throws IOException;
 }
